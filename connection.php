913c8bc5fda74fbccd45cba797daa8833de41ad5
TechWizardFaiz <abdulfaizan1818@gmail.com>
2025-01-26 23:33:29 +0530

    fixing database name

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","root","","my_db");
+$con=mysqli_connect("localhost","root","","redstream_db");
 if(mysqli_connect_error()){
     echo"<script>Cannot connect to database</script>";
     exit();
