4d6263228236f9aa7cee5a250793a8aa6db50ff5
TechWizardFaiz <abdulfaizan1818@gmail.com>
2025-01-27 23:10:34 +0530

    destroy session

--- logout.php
+++ logout.php
@@ -5,6 +5,8 @@ session_start();
 // Clear all session variables
 $_SESSION = array();
 
+// Destroy the session
+session_destroy();
 
 // Redirect the user to the login page
 header("Location: login.php");
